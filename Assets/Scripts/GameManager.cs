63ee81600ef48801d599d8a828ab7821ca80a131
Orilym <florian.millet@gmail.com>
2019-06-24 18:02:04 +0200

    StartButton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public StartGameScript StartGame;
+
+    public bool play;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (play == true)
+        {
+            StartGame.StartFunction();
+            
+
+        }
+
+    }
+
+
+    public Text MyScoreTextContent;
+    void DisplayScore()
+    {
+        // MyScoreTextContent.text = "score = " + score.ToString();
     }
 }
