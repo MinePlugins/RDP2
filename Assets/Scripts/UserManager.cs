7c21ab49bef6604ba41e03cf7d53dd7e51ca988b
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2019-06-25 11:07:25 +0200

    Login Scene

---
+++ Assets/Scripts/UserManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UserManager : MonoBehaviour
+{
+    void Login()
+    {
+    }
+
+    void Register()
+    {
+        RegisterEnum();
+    }
+
+    IEnumerator RegisterEnum()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormFileSection("username", "louis"));
+        formData.Add(new MultipartFormFileSection("password", "password1234"));
+        formData.Add(new MultipartFormFileSection("email", "louis@estiam.com"));
+
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/register.php", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+}
