79317aa68d7bd5ff1b2d94a808989ff5ebdab9a4
Orilym <florian.millet@gmail.com>
2019-06-26 18:31:03 +0200

    CoinScene

---
+++ Assets/Scenes/Point/CoinAnimScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CoinAnimScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Coin_Animation.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Animator Coin_Animation;
+    public void Anim()
+    {
+        Coin_Animation.Play("Coin_Animation");
+        Debug.Log("Anim");
+    }
+}
