5f5e0b1e0dfc850624c1a18d00f24d8b398d59ad
Mineplugins <quentin.co@free.fr>
2019-06-27 12:47:52 +0200

    ARRECO

---
+++ Assets/Scripts/ARSalleD.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class ARSalleD : MonoBehaviour, ITrackableEventHandler
+{
+
+    public TrackableBehaviour mTrackableBehaviour;
+    private EnigmeManager instance;
+    private bool isUsed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = new EnigmeManager();
+        mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            if (isUsed)
+            {
+                instance.UpScore(100);
+            }
+        }
+        else
+        {
+            Debug.Log("NOLLOLL");
+
+        }
+    }
+}
