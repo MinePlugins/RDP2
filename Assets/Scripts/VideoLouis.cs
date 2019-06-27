a4b2a6e2bda42d56d7a1218a13393102b27330e8
Orilym <florian.millet@gmail.com>
2019-06-27 17:38:01 +0200

    Videolouis

---
+++ Assets/Scripts/VideoLouis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoLouis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ARload());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public IEnumerator ARload()
+    {
+        yield return new WaitForSeconds(18);
+        SceneManager.LoadScene("AR Recognition", LoadSceneMode.Single);
+    }
+}
